d5f4db6b02288377bf00d790a5e680f6b775f11e
Vivaan5311 <76786933+Vivaan5311@users.noreply.github.com>
2021-11-21 09:10:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ noseX=0;
 noseY=0;
 
 function preload() {
-  moustache = loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE-4o3nOGv2ohkK_DYRvNKcfbaSwcrLPM26g&usqp=CAU');
+  clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
   canvas = createCanvas(300, 300);
   canvas.center();
-  video = createCapture(VIDEO)
+  video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
 
@@ -17,27 +17,24 @@ function setup() {
 }
 
 function modelLoaded() {
-  console.log('PoseNet Is Intitialized');
-}
-
-function draw() {
-image(video, 0, 0, 300, 300);
-}
-
-function take_snapshot(){
-    save('myFilterImage.png');
-}
+  console.log('PoseNet Is Initialized');
+}            
 
 function gotPoses(results)
 {
-  if(results.lenght > 0)
+  if(results.length > 0)
   {
     console.log(results);
-    console.log("nose x = " + results[0].pose.nose.x);
-    console.log("nose y = " + results[0].pose.nose.y);
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-15;
   }
-};
+}
 
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 30, 30);
+}
 
-
-
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
